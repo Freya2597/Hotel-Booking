2e7300ffe51b0fa9016cccf764adb25cb63da402
Freya Dsouza <113114228+Freya2597@users.noreply.github.com>
2024-03-28 17:01:13 -0400

    Update authMiddleware.js

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -12,7 +12,7 @@ const protect = asyncHandler(async(req, res, next) => {
     ) {
         try{
             token = req.headers.authorization.split(' ')[1]
-            const decode = jwt.verify(token, process.env.JWT_SECRET || "abc123")
+            const decode = jwt.verify(token, process.env.JWT_SECRET)
             req.user = await User.findById(decode.id).select('-password')
         } catch (error) {
             console.error(error)
